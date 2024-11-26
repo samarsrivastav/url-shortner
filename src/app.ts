8e755a0d41f7cb4f370fb1d989a39104863df228
samarsrivastav <samarsrivastav69@gmail.com>
2024-11-26 16:16:51 +0530

    cors

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import urlRoutes from './routes/urlRoutes';
 import { rateLimiter } from './middleware/rateLimiter';
-
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(rateLimiter);
 app.use('/', urlRoutes);
